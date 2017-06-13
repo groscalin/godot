9b4225fd77243e0a0ace7d1cc3a99e68fb4563c4
Juan Linietsky <reduzio@gmail.com>
2017-06-13 01:23:04 -0300

    -Fixed occluder rendering, closes #8560 -Ability to smooth out 2D shadow filters

--- drivers/gles3/rasterizer_storage_gles3.h
+++ drivers/gles3/rasterizer_storage_gles3.h
@@ -1286,6 +1286,7 @@ public:
 
 	struct CanvasOccluder : public RID_Data {
 
+		GLuint array_id; // 0 means, unconfigured
 		GLuint vertex_id; // 0 means, unconfigured
 		GLuint index_id; // 0 means, unconfigured
 		PoolVector<Vector2> lines;
